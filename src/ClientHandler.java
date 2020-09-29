d0b19e019742624469efa3ee1bd98f998287c383
patricknogaj <patricknogaj@gmail.com>
2020-09-29 12:30:06 -0400

    consolidation
    aaa

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -22,9 +23,6 @@ import java.util.TimeZone;
 public class ClientHandler extends Thread {
 	
 	private Socket socket;
-	private File file;
-	private int fileLength;
-	byte[] payload;
 	
 	//HashMap value of {COMMAND_NAME, SUPPORTED {1: true, 0: false}
 	private final HashMap<String, Integer> COMMANDS = new HashMap<String, Integer>() {{
@@ -48,17 +46,61 @@ public class ClientHandler extends Thread {
 			PrintStream out = new PrintStream(socket.getOutputStream());
 			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 			DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+			
 			String requestLine = reader.readLine();
 			String modifiedDate = reader.readLine();
+			String[] header = requestLine.split("\\s+");
 			
-			out.print(writeMessage(requestLine, modifiedDate));
-			
-			if(payload != null)
-				outStream.write(payload);
-			
+			if(header.length != 3) {
+				out.print(getResponse(400));
+			} else {
+				if(!COMMANDS.containsKey(header[0])) {
+					out.print(getResponse(400));
+				} else if(COMMANDS.containsKey(header[0]) && COMMANDS.get(header[0]) == 0) {
+					out.print(getResponse(501));
+				} else if(!header[2].equals("HTTP/1.0")) {
+					out.print(getResponse(505));
+				} else {
+					File file = new File(header[1].substring(1, header[1].length())).getAbsoluteFile();
+					int fileLength = (int) file.length();
+					
+					if(!file.isFile()) {
+						out.print(getResponse(404));
+					} else if(!file.canRead()){
+						out.print(getResponse(403));
+					} else {
+						long lastModified = file.lastModified();
+						Date date = new Date(lastModified);
+						Date currentDate = new Date();
+						SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM YYYY HH:mm:ss zzz");
+						dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+						byte[] payload = readFileData(file, fileLength);
+						
+						if(modifiedDate.length() > 0) {
+							if(modifiedDate.substring(19, modifiedDate.length()).length() > 28) {
+								Date ifModifiedDate = new Date(modifiedDate.substring(19, modifiedDate.length()));
+							
+								if(date.compareTo(ifModifiedDate) == -1) {
+									if(!header[0].contains("HEAD")) {
+										out.print(getResponse(304));
+										out.print("Expires: " + dateFormat.format(new Date(currentDate.getYear() + 1, currentDate.getMonth(), currentDate.getDay()))+ "\r\n\r\n");
+									}
+								} 
+							}
+						}
+						out.print(getResponse(200));
+						out.print("Content-Type: " + contentType(header[1]) + "\r\n" +
+								  "Content-Length: " + fileLength + "\r\n" +
+								  "Last-Modified: " + dateFormat.format((new Date(lastModified))) + "\r\n" +
+								  "Content-Encoding: identity" + "\r\n" +
+								  "Allow: GET, POST, HEAD" + "\r\n" +
+								  "Expires: " + dateFormat.format(new Date(currentDate.getYear() + 1, currentDate.getMonth(), currentDate.getDay()))+ "\r\n\r\n");
+						outStream.write(payload);
+					}
+				}
+			}
 			outStream.flush();
 			outStream.close();
-			
 			out.flush();
 			out.close();
 			inputStream.close();
@@ -66,56 +108,6 @@ public class ClientHandler extends Thread {
 			socket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-	}
-	
-	public String writeMessage(String requestLine, String modifiedDate) throws IOException {
-		String[] header = requestLine.split("\\s+");
-		if(header.length != 3) {
-			return "HTTP/1.0 400 " + getResponse(400);
-		} else {
-			if(!COMMANDS.containsKey(header[0])) {
-				return "HTTP/1.0 400 " + getResponse(400);
-			} else if(COMMANDS.containsKey(header[0]) && COMMANDS.get(header[0]) == 0) {
-				return "HTTP/1.0 501 " + getResponse(501);
-			} else if(!header[2].equals("HTTP/1.0")) {
-				return "HTTP/1.0 505 " + getResponse(505);
-			} else {
-				file = new File(header[1].substring(1, header[1].length())).getAbsoluteFile();
-				fileLength = (int) file.length();
-				if(!file.isFile()) {
-					return "HTTP/1.0 404 " + getResponse(404);
-				} else if(!file.canRead()){
-					System.out.println(requestLine + " is forbidden!");
-					return "HTTP/1.0 403 " + getResponse(403);
-				} else {
-					long lastModified = file.lastModified();
-					Date date = new Date(lastModified);
-					Date currentDate = new Date();
-					SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM YYYY HH:mm:ss zzz");
-					dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-					payload = readFileData(file, fileLength);
-					
-					if(modifiedDate.length() > 0) {
-						if(modifiedDate.substring(19, modifiedDate.length()).length() > 28) {
-							Date ifModifiedDate = new Date(modifiedDate.substring(19, modifiedDate.length()));
-						
-							if(date.compareTo(ifModifiedDate) == -1) {
-								if(!header[0].contains("HEAD"))
-									return "HTTP/1.0 304 " + getResponse(304) + "\r\n" + 
-									   	"Expires: " + dateFormat.format(new Date(currentDate.getYear() + 1, currentDate.getMonth(), currentDate.getDay()))+ "\r\n\r\n";
-							}
-						}
-					}
-					return "HTTP/1.0 200 " + getResponse(200) + "\r\n" + 
-							"Content-Type: " + contentType(header[1]) + "\r\n" +
-							"Content-Length: " + fileLength + "\r\n" +
-							"Last-Modified: " + dateFormat.format((new Date(lastModified))) + "\r\n" +
-							"Content-Encoding: identity" + "\r\n" +
-							"Allow: GET, POST, HEAD" + "\r\n" +
-							"Expires: " + dateFormat.format(new Date(currentDate.getYear() + 1, currentDate.getMonth(), currentDate.getDay()))+ "\r\n\r\n";
-				}
-			}
 		}
 	}
 	
@@ -140,25 +132,25 @@ public class ClientHandler extends Thread {
 	 */
 	public String getResponse(int code) {
 		if(code == 200)
-			return "OK";
+			return "HTTP/1.0 200 OK\r\n";
 		if(code == 304)
-			return "Not Modified";
+			return "HTTP/1.0 304 Not Modified\r\n";
 		if(code == 400)
-			return "Bad Request";
+			return "HTTP/1.0 400 Bad Request\r\n";
 		if(code == 403)
-			return "Forbidden";
+			return "HTTP/1.0 403 Forbidden\r\n";
 		if(code == 404)
-			return "Not Found";
+			return "HTTP/1.0 404 Not Found\r\n";
 		if(code == 408)
-			return "Request Timeout";
+			return "HTTP/1.0 408 Request Timeout\r\n";
 		if(code == 500)
-			return "Internal Server Error";
+			return "HTTP/1.0 500 Internal Server Error\r\n";
 		if(code == 501)
-			return "Not Implemented";
+			return "HTTP/1.0 501 Not Implemented\r\n";
 		if(code == 503)
-			return "Service Unavailable";
+			return "HTTP/1.0 503 Service Unavailable\r\n";
 		if(code == 505)
-			return "HTTP Version Not Supported";
+			return "HTTP/1.0 505 HTTP Version Not Supported\r\n";
 		return " ";
 	}
 	
