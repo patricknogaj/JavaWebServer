670525e8bbf647d20ad51d2ceecb145441f825f4
patricknogaj <patricknogaj@gmail.com>
2020-10-02 15:58:12 -0400

    implemented executor, did not incorp 503

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,7 +17,7 @@ import java.util.TimeZone;
 /**
  * Rutgers University -- Information Technology CS352
  * @author Patrick Nogaj (NET ID: pn220) 
- * 	   Stephen Fu (NET ID: svf13) 
+ *		   Stephen Fu (NET ID: svf13) 
  *         Jayson Pitta (NET ID: jrp289)
  * 
  */
@@ -24,7 +25,6 @@ import java.util.TimeZone;
 public class ClientHandler extends Thread {
 	
 	private Socket socket;
-	private static int numConnections = 0;
 	
 	//HashMap value of {COMMAND_NAME, SUPPORTED {1: true, 0: false}
 	private final HashMap<String, Integer> COMMANDS = new HashMap<String, Integer>() {{
@@ -43,98 +43,97 @@ public class ClientHandler extends Thread {
 	 */
 	public ClientHandler(Socket socket) {
 		this.socket = socket;
-		numConnections++;
-		start();
+		run();
 	}
 	
 	public void run() {
 		try {
-			InputStream inputStream = socket.getInputStream();
-			PrintStream out = new PrintStream(socket.getOutputStream());
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-			DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
-
-		if (numConnections > 50) {
-				out.print(getResponse(503));
+			if(!socket.isClosed()) {
+				InputStream inputStream = socket.getInputStream();
+				PrintStream out = new PrintStream(socket.getOutputStream());
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+				DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+				
+				socket.setSoTimeout(5000);
+				try {
+					if(inputStream == null || reader == null)
+						return;
+					
+					String requestLine = reader.readLine();
+					String modifiedDate = reader.readLine();
+					
+					if(requestLine == null)
+						return;
+					
+					String[] header = requestLine.split("\\s+");
+					
+					if(header.length != 3) {
+						out.print(getResponse(400));
+					} else {
+						if(!COMMANDS.containsKey(header[0])) {
+							out.print(getResponse(400));
+						} else if(COMMANDS.containsKey(header[0]) && COMMANDS.get(header[0]) == 0) {
+							out.print(getResponse(501));
+						} else if(!header[2].equals("HTTP/1.0")) {
+							out.print(getResponse(505));
+						} else {
+							File file = new File(header[1].substring(1, header[1].length())).getAbsoluteFile();
+							int fileLength = (int) file.length();
+							
+							if(!file.isFile()) {
+								out.print(getResponse(404));
+							} else if(!file.canRead()){
+								out.print(getResponse(403));
+							} else {
+								Date lastModifiedDate = new Date(file.lastModified());
+								Date currentDate = new Date();
+								currentDate.setYear(currentDate.getYear() + 1);
+								SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM YYYY HH:mm:ss zzz");
+								dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+								byte[] payload = readFileData(file, fileLength);
+								
+								if(modifiedDate.length() > 0) {
+									if(modifiedDate.substring(19, modifiedDate.length()).length() > 28) {
+										Date ifModifiedDate = new Date(modifiedDate.substring(19, modifiedDate.length()));
+									
+										if(lastModifiedDate.compareTo(ifModifiedDate) == -1) {
+											if(!header[0].contains("HEAD")) {
+												out.print(getResponse(304));
+												out.print("Expires: " + dateFormat.format(currentDate)+ "\r\n\r\n");
+											}
+										} 
+									}
+								}
+								out.print(getResponse(200));
+								out.print("Content-Type: " + contentType(header[1]) + "\r\n" +
+										  "Content-Length: " + fileLength + "\r\n" +
+										  "Last-Modified: " + dateFormat.format(lastModifiedDate) + "\r\n" +
+										  "Content-Encoding: identity" + "\r\n" +
+										  "Allow: GET, POST, HEAD" + "\r\n" +
+										  "Expires: " + dateFormat.format(currentDate)+ "\r\n\r\n");
+								if(header[0].equals("GET") || header[0].equals("POST"))
+									outStream.write(payload);
+							}
+						}
+					}
+				} catch (SocketTimeoutException se) {
+					out.print(getResponse(408));
+				}
+				
+				outStream.flush();
+				out.flush();
+				
+				Thread.sleep(250); //wait for 0.25s after flush to close out everything else.
+				
 				outStream.close();
 				out.close();
 				inputStream.close();
 				reader.close();
-				socket.close();
-				numConnections--;
-				return;
-			}
-			
-			socket.setSoTimeout(5000);
-			try {
-				String requestLine = reader.readLine();
-				String modifiedDate = reader.readLine();
-				String[] header = requestLine.split("\\s+");
-				
-				if(header.length != 3) {
-					out.print(getResponse(400));
-				} else {
-					if(!COMMANDS.containsKey(header[0])) {
-						out.print(getResponse(400));
-					} else if(COMMANDS.containsKey(header[0]) && COMMANDS.get(header[0]) == 0) {
-						out.print(getResponse(501));
-					} else if(!header[2].equals("HTTP/1.0")) {
-						out.print(getResponse(505));
-					} else {
-						File file = new File(header[1].substring(1, header[1].length())).getAbsoluteFile();
-						int fileLength = (int) file.length();
-						
-						if(!file.isFile()) {
-							out.print(getResponse(404));
-						} else if(!file.canRead()){
-							out.print(getResponse(403));
-						} else {
-							Date lastModifiedDate = new Date(file.lastModified());
-							Date currentDate = new Date();
-							currentDate.setYear(currentDate.getYear() + 1);
-							SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM YYYY HH:mm:ss zzz");
-							dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-							byte[] payload = readFileData(file, fileLength);
-							
-							if(modifiedDate.length() > 0) {
-								if(modifiedDate.substring(19, modifiedDate.length()).length() > 28) {
-									Date ifModifiedDate = new Date(modifiedDate.substring(19, modifiedDate.length()));
-								
-									if(lastModifiedDate.compareTo(ifModifiedDate) == -1) {
-										if(!header[0].contains("HEAD")) {
-											out.print(getResponse(304));
-											out.print("Expires: " + dateFormat.format(currentDate)+ "\r\n\r\n");
-										}
-									} 
-								}
-							}
-							out.print(getResponse(200));
-							out.print("Content-Type: " + contentType(header[1]) + "\r\n" +
-									  "Content-Length: " + fileLength + "\r\n" +
-									  "Last-Modified: " + dateFormat.format(lastModifiedDate) + "\r\n" +
-									  "Content-Encoding: identity" + "\r\n" +
-									  "Allow: GET, POST, HEAD" + "\r\n" +
-									  "Expires: " + dateFormat.format(currentDate)+ "\r\n\r\n");
-							if(header[0].equals("GET") || header[0].equals("POST"))
-								outStream.write(payload);
-						}
-					}
-				}
-			} catch (SocketTimeoutException se) {
-				out.print(getResponse(408));
-			}
-			
-			outStream.flush();
-			out.flush();
-			
-			Thread.sleep(250); //wait for 0.25s after flush to close out everything else.
-			
-			outStream.close();
-			out.close();
-			inputStream.close();
-			reader.close();
+			} 
+			System.out.println("closing socket..." + socket);
 			socket.close();
-			numConnections--;
+		} catch (SocketException se) {
+			se.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/PartialHTTP1Server.java
+++ src/PartialHTTP1Server.java
@@ -1,10 +1,14 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Rutgers University -- Information Technology CS352
  * @author Patrick Nogaj (NET ID: pn220) 
- * 	   Stephen Fu (NET ID: svf13) 
+ * 		   Stephen Fu (NET ID: svf13) 
  *         Jayson Pitta (NET ID: jrp289)
  * 
  */
@@ -17,12 +21,17 @@ public class PartialHTTP1Server {
 	 * @param args :: arg[0] -> port number (integer)
 	 */
 	public static void main(String[] args) {
-		
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(50, 50, 5, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
 		int port = 0;
 		
 		if(args.length > 0) {
 			try {
 				port = Integer.parseInt(args[0]);
+				
+				if(port < 0 || port > 65535) {
+					System.out.println("ERR: Port number out of range...closing program");
+					System.exit(1);
+				}
 			} catch (NumberFormatException e) {
 				System.err.println("ERR: Argument 0 must be a valid integer.");
 				System.exit(1);
@@ -35,12 +44,14 @@ public class PartialHTTP1Server {
 		try(ServerSocket server = new ServerSocket(port)) {
 			System.out.println("Server is listening on port " + port);
 			while(true) {
-				Socket socket = server.accept();
-				new ClientHandler(socket);
+				Socket socket = server.accept();				
+				executor.execute(new ClientHandler(socket));
 			}
+		} catch (RejectedExecutionException ex) {
+			System.err.println("Rejected: " + ex.getMessage());
 		} catch (Exception e) {
 			System.err.println("ERR: " + e.getMessage());
 		}
+			executor.shutdown();
 	}
-
 }
